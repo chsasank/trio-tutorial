c24a9dba6db0a241b7f95cda2a708135f5a79c61
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2021-01-22 17:12:43 +0530

    cide

--- chat_server.py
+++ chat_server.py
@@ -3,11 +3,13 @@ import trio
 
 class ChatServer:
     def __init__(self):
+        # place to store list of current users
+        # this is why we used a class instead of a function
         self.users = {}
 
     async def server(self, server_stream):
         await server_stream.send_all(b"<meta>: Please enter username: ")
-        user_name = None
+        current_user_name = None
         async for data in server_stream:
             if server_stream not in self.users.values():
                 # handle login
@@ -17,30 +19,32 @@ class ChatServer:
                         b"<meta>: Username taken. Please enter another: "
                     )
                 else:
-                    user_name = proposed_user_name
-                    self.users[user_name] = server_stream
-                    for other_user_name, other_user_stream in self.users.items():
-                        if other_user_name == user_name:
-                            await other_user_stream.send_all(
-                                f"<meta>: Welcome to the chat, {user_name}.\n".encode()
+                    current_user_name = proposed_user_name
+                    self.users[current_user_name] = server_stream
+                    for user_name, user_stream in self.users.items():
+                        if user_name == current_user_name:
+                            await user_stream.send_all(
+                                f"<meta>: Welcome {current_user_name}.\n".encode()
                             )
                         else:
-                            await other_user_stream.send_all(
-                                f"<meta>: {user_name} joined the chat.\n".encode()
+                            await user_stream.send_all(
+                                f"<meta>: {current_user_name} joined.\n".encode()
                             )
 
             else:
                 # broadcast the data to other users
-                for other_user_name, other_user_stream in self.users.items():
-                    if other_user_name != user_name:
-                        await other_user_stream.send_all(
-                            f"<{user_name}> : ".encode() + data
+                for user_name, user_stream in self.users.items():
+                    if user_name != current_user_name:
+                        await user_stream.send_all(
+                            f"<{current_user_name}> : ".encode() + data
                         )
 
         try:
-            del self.users[user_name]
-            for other_user_name, other_user_stream in self.users.items():
-                await other_user_stream.send_all(f"<meta>: {user_name} left\n".encode())
+            del self.users[current_user_name]
+            for user_name, user_stream in self.users.items():
+                await user_stream.send_all(
+                    f"<meta>: {current_user_name} left\n".encode()
+                )
         except KeyError:
             pass
 
